80dc59ff3994812b9ca128440fad6edca1de24da
anas derk <anas.derk2020@gmail.com>
2024-11-20 20:57:55 +0300

    Fix

--- models/products.model.js
+++ models/products.model.js
@@ -258,10 +258,12 @@ async function getRelatedProductsInTheProduct(productId, language) {
             return {
                 msg: getSuitableTranslations("Get Sample From Related Products In This Product Process Has Been Successfuly !!", language),
                 error: false,
-                data: await productModel.aggregate([
+                data: await productModel.populate((await productModel.aggregate([
                     { $match: { categories: productInfo.categories, _id: { $ne: new mongoose.Types.ObjectId(productId) } } },
                     { $sample: { size: 10 } }
-                ]),
+                ])), {
+                    path: "categories",
+                }),
             }
         }
         return {
