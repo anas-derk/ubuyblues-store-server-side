efb9502ac34be323fc26c52eb1f330d460922e51
anas-derk <anas.derk2023@gmail.com>
2025-03-29 22:25:21 +0300

    Test

--- controllers/ads.controller.js
+++ controllers/ads.controller.js
@@ -33,6 +33,7 @@ async function postNewTextAd(req, res) {
         res.json(result);
     }
     catch (err) {
+        console.log(err)
         res.status(500).json(getResponseObject(getSuitableTranslations("Internal Server Error !!", req.query.language), true, {}));
     }
 }
