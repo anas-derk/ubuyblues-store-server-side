cd6aae3e95e313da7f9af9cc121cf4097bebde4b
anas-derk <anas.derk2023@gmail.com>
2025-03-30 01:45:32 +0300

    Test

--- controllers/stores.controller.js
+++ controllers/stores.controller.js
@@ -121,6 +121,7 @@ async function postNewStore(req, res) {
         res.json(result);
     }
     catch (err) {
+        console.log(err);
         res.status(500).json(getResponseObject(getSuitableTranslations("Internal Server Error !!", req.query.language), true, {}));
     }
 }
